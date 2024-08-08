5e6aaa666034c5d838ed7210c090ca4c62e4a147
seoariseo <seoari704@gmail.com>
2024-08-08 17:37:34 +0900

    fluer_2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+
 $(function () {
+
+
+    //메뉴 영역
+
     $(".menu_btn").click(function () {
         $(this).toggleClass("act")
     }) //menu_btn.click
 
+
+
+    //slick 영역
 
     $(".m_img").slick({
         autoplay: true,
@@ -19,7 +27,35 @@ $(function () {
             }
         } ]
 
+    })//slick
+
+
+
+
+
+
+
+
+    // best flower 영역
+    
+    $(".bf_img_list > li ").hover(function(){
+        $(this).siblings().css({opacity : "0.5"})
+        $(".bf_sub").hide();
+        $(this).find(".bf_sub").slideDown();
+        
+    },function(){
+        $(".bf_img_list > li" ).css({opacity : "1"});
+        $(".bf_sub").stop().slideUp();
     })
+
+    $(".bf_sub").hover(function(){
+        $(this).stop();
+    },function(){
+        $(".bf_sub").slideUp();
+    })//
+
+    
+
 
 
 
